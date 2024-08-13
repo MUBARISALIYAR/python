f9f3392fd2d7e38ac6865fb10e4c9597d741434c
MUBARISALIYAR <aliyarmubaris@gmail.com>
2024-08-13 13:35:25 +0530

    python

---
+++ demo.py
@@ -0,0 +1,7 @@
+name=input("enter your name = ")
+age=int(input("enter age = "))
+weight=float(input("enter weight ="))
+
+print("name=",name)
+print("age=",age)
+print("wight=",weight)
---
+++ operators.py
@@ -0,0 +1,97 @@
+                                        # ARITHMETIC OPERATORS
+
+# addition
+
+print(10+5)
+print('mubz'+'mubz')
+
+# substraction
+
+print(10-5)
+
+# multiplicaton
+
+print(10*5)
+print('mubz '*3)
+
+# division 
+ 
+print(10/3)
+
+# floor division
+
+print(10//3)
+
+# exponential
+
+print(10**2)
+
+# modulus
+
+print(10%3)
+
+                                            # ASSIGNMENT OPERATORS
+
+
+# EQUAL To 
+
+a=10
+print(a)
+
+# plus equal to   A=A+5
+
+a=10
+a+=5
+print(a)
+
+# minus equal to   A=A-5
+
+a=10
+a-=5
+print(a)
+
+# star equal to   A=A*5
+
+a=10
+a*=5
+print(a)
+
+# slash equal to   A=A/5
+
+a=10
+a/=5
+print(a)
+
+# modulus equal to   A=A%5
+
+a=10
+a%=3
+print(a)
+
+                                            # COMPARISON OPERATORS
+
+# eqaul to 
+
+print(10==10)
+print('apple'=='apple')
+
+
+# not equalto 
+
+print(10!=10)
+
+# greater than 
+
+print(10>5)
+
+# less than 
+ 
+print(10<5)
+
+# less than or equal to 
+
+print(10<=15)
+
+# greater than or equal to 
+
+print(10>=10)
